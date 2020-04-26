91f471f44fd80cf3523d8e583bac41c5002cf3c7
Dovydas <dovydastt@gmail.com>
2020-04-26 16:13:10 +0300

    first commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,12 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = {"src/test/Feature/openValorantStream.feature"},plugin = {"pretty"},strict = true)
+
+public class Runner {
+
+
+
+}
---
+++ src/test/Feature/openValorantStream.feature
@@ -0,0 +1,6 @@
+Feature: Watch valorant stream on twitch.tv
+
+  Scenario:  Open first valorant stream on www.twitch.tv
+    Given I am on valorant game section page
+    When I click on first stream
+    Then I see first selected stream
